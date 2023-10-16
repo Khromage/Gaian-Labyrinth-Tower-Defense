30ab98eb188577d2d59574dcefcb98f802ce2f3b
Christian Perez <Christian.perez.935@my.csun.edu>
2023-10-15 21:57:55 -0700

    Added free roam and ADS camera

--- Gaian Labyrinth Tower Defense/Assets/Scripts/Player.cs
+++ Gaian Labyrinth Tower Defense/Assets/Scripts/Player.cs
@@ -4,36 +4,101 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start() { 
-    
-        
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("KeyBinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    //Method to be checked on first frame of the game
+    public void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
     }
 
-    // Update is called once per frame
-    private void Update() {
-        playerMove();
+    //Method to be checked on every frame of the game
+    public void Update() {
+        //Checking if player is on the ground
+        grounded = Physics.Raycast(transform.position + new Vector3(0, 0.05f, 0), Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
+        getUserInput();
+        playerSpeedControl();
+
+        //handling drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
     }
 
-    private void playerMove() {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-        inputVector = inputVector.normalized;
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir;
-        Debug.Log(inputVector);
+    public void FixedUpdate() {
+        movePlayer();
     }
+
+    private void getUserInput() {
+        //Player hits WASD
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Player wants to jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded) {
+            readyToJump = false;
+            jump();
+            Invoke(nameof(resetJump), jumpCooldown);
+        } 
+    }
+
+    private void movePlayer() {
+        //calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        //if on the ground
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void playerSpeedControl() {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //Limit player's movement velocity when reaching max speed
+        if(flatVel.magnitude > moveSpeed) {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed; 
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void jump() {
+        //reset Y velocity to prepare for new jump
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void resetJump() {
+        readyToJump = true;
+    }
+
 }
