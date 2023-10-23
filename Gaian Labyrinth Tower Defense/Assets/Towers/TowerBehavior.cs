b5f1a237553ee7956c266525229a86264777ad3d
Khromage <khromage@gmail.com>
2023-10-22 21:19:07 -0700

    Bullet Firing and Seeking

--- Gaian Labyrinth Tower Defense/Assets/Towers/TowerBehavior.cs
+++ Gaian Labyrinth Tower Defense/Assets/Towers/TowerBehavior.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class TowerBehavior : MonoBehaviour
 {
     public Transform target;
-    public float range = 5f;
+
+    [Header("Tower Stats")]
+    
+    public float range = 10f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Fields")]
+
     public string enemyTag = "Enemy";
+
     public Transform partToRotate;
     public float turnSpeed = 5f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+
 
     // Call the targeting function twice a second to scan for enemies
     void Start()
@@ -53,6 +67,23 @@ public class TowerBehavior : MonoBehaviour
         // Using Lerp to smooth transition between target swaps instead of snapping to new targets
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, targetingRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGO = Instantiate (bulletPrefab, firePoint.position, firePoint.rotation);
+        BulletBehavior bullet = bulletGO.GetComponent<BulletBehavior>();
+
+        if (bullet != null)
+            bullet.Seek(target);
     }
 
     // Tower range visualization via gizmos 
