723207fff34eb54887996252dbb5e19d88dd0bbf
MayonnaiseDays <is.park2001@gmail.com>
2023-10-30 19:13:03 -0700

    more gun stuff

--- Gaian Labyrinth Tower Defense/Assets/Code/Scripts/Gun.cs
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/Gun.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [Header("Poggers")]
+
+    public UnityEvent OnGunShoot;
+    public float FireCooldown;
+
+    //default is semi
+    public bool Automatic;
+
+    private float CurrentCooldown;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //not neccessary but puts gun on cd on game start
+        CurrentCooldown = FireCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Automatic)
+        {
+            //if mouse held down
+            if(Input.GetMouseButton(0))
+            {
+                //if cd is 0 or less
+                if (CurrentCooldown <= 0f)
+                {
+                    //go on cd again
+                    OnGunShoot?.Invoke();
+                    CurrentCooldown = FireCooldown;
+                }
+            }
+        }
+        else
+        {
+            //if mouse pressed
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (CurrentCooldown <= 0f)
+                {
+                    OnGunShoot?.Invoke();
+                    CurrentCooldown = FireCooldown;
+                }
+            }
+        }
+
+        //lower the cd
+        CurrentCooldown -= Time.deltaTime;
     }
 }
--- Gaian Labyrinth Tower Defense/Assets/Code/Scripts/GunDamage.cs
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/GunDamage.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class GunDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Damage;
+    public float BulletRange;
+    private Transform PlayerCamera;
+    
+    RaycastHit hit;
+    public GameObject Laser;
+    
+    public float FireCooldown;
+
+    public bool Automatic;
+    private float CurrentCooldown;
+
+   
     void Start()
     {
-        
+        PlayerCamera = Camera.main.transform;
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Automatic)
+        {
+            //if mouse held down
+            if(Input.GetMouseButton(0))
+            {
+                //if cd is 0 or less
+                if (CurrentCooldown <= 0f)
+                {
+                    //go on cd again
+                    Shoot();
+                    CurrentCooldown = FireCooldown;
+                }
+            }
+        }
+        else
+        {
+            //if mouse pressed
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (CurrentCooldown <= 0f)
+                {
+                    Shoot();
+                    CurrentCooldown = FireCooldown;
+                }
+            }
+        }
+
+        //lower the cd
+        CurrentCooldown -= Time.deltaTime;
+    }
+
+
+    // Update is called once per frame
+    public void Shoot()
+    {
+        Ray gunRay = new Ray(transform.position, transform.forward);
         
+        if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
+        {
+Instantiate(Laser, transform.position, transform.rotation);
+            if (hitInfo.collider.gameObject.tag == "Enemy")
+            {
+                EnemyBehavior e = hitInfo.collider.gameObject.GetComponent<EnemyBehavior>();
+                e.takeDamage(Damage, gameObject);
+                //hitInfo.collider.gameObject.takeDamage(10);
+            }
+        }
+        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 200, Color.yellow);
     }
 }
