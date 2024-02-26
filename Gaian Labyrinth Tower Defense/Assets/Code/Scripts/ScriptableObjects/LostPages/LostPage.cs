f500d9a9a8fbf693cb276253f5d8b1a10f51a68b
Jorge Torrez <cocotorrez1974@gmail.com>
2024-02-25 18:32:18 -0800

    Lost Pages Scriptable Object

---
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/ScriptableObjects/LostPages/LostPage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLostPage", menuName = "Pages/Lost Page", order = 0)]
+public class LostPage : ScriptableObject
+{
+    public string pageTitle;
+    [TextArea(10, 20)]
+    public string pageContent;
+    public Sprite pageImage; // visuals for later.
+}
