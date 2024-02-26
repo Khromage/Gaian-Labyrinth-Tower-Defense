e41cdc55f0e07c02128ef28d9f477bbac9911dd4
Kelron1337 <dawsonj.ford@gmail.com>
2024-02-25 23:25:35 -0800

    UI Changes

--- Gaian Labyrinth Tower Defense/Assets/Code/Scripts/OptionsMenu.cs
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/OptionsMenu.cs
@@ -1,36 +1,84 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
-    Resolution[] resolutions;
+    UnityEngine.Resolution[] resolutions;
 
-    void Start () {
+    public TMP_Dropdown FPSDropdown;
+
+    void Start()
+    {
         resolutions = Screen.resolutions;
+        PopulateResolutionDropdown();
+
+    }
+
+    private void PopulateResolutionDropdown()
+    {
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
-        for (int i = 0; i < resolutions.Length; i++) {
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
+
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
-    public void SetVolume (float volume) {
+
+    public void SetVolume(float volume)
+    {
         audioMixer.SetFloat("Volume", volume);
     }
 
-    public void SetQuality(int qualityIndex) {
+    public void SetQuality(int qualityIndex)
+    {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
-    public void SetFullscreen (bool isFullscreen) {
+    public void SetFullscreen(bool isFullscreen)
+    {
         Screen.fullScreen = isFullscreen;
     }
-  
+
+    public void SetFPS(int f) {
+        Application.targetFrameRate = f;
+        
+    }
+
+    private void OnFPSDropdownValueChanged(int index)
+    {
+        string selectedText = FPSDropdown.options[index].text;
+
+        // Try to parse the selected text to an integer
+        if (int.TryParse(selectedText, out int fpsValue))
+        {
+            // Call SetFPS with the parsed integer value
+            SetFPS(fpsValue);
+        }
+        else
+        {
+            Debug.LogWarning("Failed to parse FPS value from dropdown: " + selectedText);
+        }
+    }
+
+    void Update() {
+        
+    }
 }
