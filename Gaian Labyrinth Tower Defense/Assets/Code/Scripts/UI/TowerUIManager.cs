c0bc135eff872dcab525360c18e37d97d7b38c7b
Khromage <94722022+Khromage@users.noreply.github.com>
2024-03-01 22:35:57 -0800

    sync

--- Gaian Labyrinth Tower Defense/Assets/Code/Scripts/UI/TowerUIManager.cs
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/UI/TowerUIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class TowerUIManager : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class TowerUIManager : MonoBehaviour
     public TMP_Text towerName;
     public TMP_Text towerValue;
     public TMP_Text towerDescription;
-    public GameObject[] optionTiles;
+    public UpgradeOption[] upgradeOptions;
     
     public void OnEnable()
     {
@@ -23,14 +24,39 @@ public class TowerUIManager : MonoBehaviour
         towerValue.text = "Value: " + selectedTower.cost;
         towerDescription.text = selectedTower.GetDescription();
 
+        for(int i=0; i<upgradeOptions.Length; i++)
+            upgradeOptions[i].Tile.SetActive(false);
 
-        for(int i=0; i<optionTiles.Length; i++)
-            optionTiles[i].SetActive(false);
         // check tower level and display appropriate # of upgrade options
         if(selectedTower.currentLevel == 1)
         {
-            optionTiles[1].SetActive(true);
-            
+            upgradeOptions[1].Tile.SetActive(true);
+            upgradeOptions[1].SetOptionInfo(0, selectedTower);
+        } 
+        else if(selectedTower.currentLevel == 2)
+        {
+            for(int i=0; i<upgradeOptions.Length; i++)
+            {
+                upgradeOptions[i].Tile.SetActive(true);
+                upgradeOptions[i].SetOptionInfo(i+1, selectedTower);
+            }
         }
+    }
+}
+
+[Serializable]
+public struct UpgradeOption
+{
+    public GameObject Tile;
+    public Image Icon; // placeholder
+    public TMP_Text Name;
+    public TMP_Text Description;
+    public TMP_Text Cost;
+
+    public void SetOptionInfo(int branch, TowerBehavior selectedTower)
+    {
+        Name.text = selectedTower.towerInfo.Branches[branch].Name;
+        Cost.text = selectedTower.towerInfo.Branches[branch].Cost.ToString();
+        Description.text = selectedTower.towerInfo.Branches[branch].Description;
     }
 }
