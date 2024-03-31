570ed6b9bdfd1a7f5a95583c067553f000131f47
Jorge Torrez <cocotorrez1974@gmail.com>
2024-03-31 12:01:10 -0700

    Encyclopedia Displaying Lost Pages

--- Gaian Labyrinth Tower Defense/Assets/Code/Scripts/CampaignMenu/Encyclopedia.cs
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/CampaignMenu/Encyclopedia.cs
@@ -16,6 +16,7 @@ public class Encyclopedia : MonoBehaviour
     [SerializeField] private TMPro.TextMeshProUGUI titleText;
     [SerializeField] private TMPro.TextMeshProUGUI contentText;
     [SerializeField] private Image pageImage; 
+    private int currentPageIndex = 0;
 
     public GameObject indexView; 
     public GameObject pageView; 
@@ -63,6 +64,42 @@ public void CloseEncyclopedia()
             Debug.LogError("Lost page with id " + id + " not found.");
         }
     }
+    public void DisplayNextPage()
+{
+    // Check if the next page exists; if not, wrap around or reset to 0
+    if (currentPageIndex >= lostPages.Length - 1)
+    {
+        currentPageIndex = 0; // Reset to the first page or handle as needed
+    }
+    else
+    {
+        currentPageIndex++; // Move to the next page
+    }
+    
+    DisplayEncyclopediaInfo(currentPageIndex);
+}
+public void DisplayPreviousPage()
+{
+    // Check if there is a previous page; if not, wrap around or set to the last page
+    if (currentPageIndex <= 0)
+    {
+        currentPageIndex = lostPages.Length - 1; // Move to the last page
+    }
+    else
+    {
+        currentPageIndex--; // Move to the previous page
+    }
+    
+    DisplayEncyclopediaInfo(currentPageIndex);
+}
+public void OnPrevioustPageButtonClicked()
+{
+    DisplayPreviousPage();
+}
+public void OnNextPageButtonClicked()
+{
+    DisplayNextPage();
+}
     public void OnLostPageButtonClicked(int pageId)
     {
          Debug.Log($"Lost Page Button Clicked: {pageId}");
