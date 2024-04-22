d10a6dfc887b638337014d4a50534ef196ccc9a3
MayonnaiseDays <is.park2001@gmail.com>
2024-04-21 17:10:23 -0700

    wip tech tree

---
+++ Gaian Labyrinth Tower Defense/Assets/Code/Scripts/Tech Stuff/Technology.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "New Tech")]
+public class Technology : ScriptableObject
+{
+    public string Definition;
+    //public 
+}
